91d9c491a2eb12b91d548c6f40bbdd5d71429f27
Prabesh <iprabesh@hotmail.com>
2019-01-28 07:47:45 -0600

    First Version

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,9 @@
 class FavoritesController < ApplicationController
-    before_action :require_signin
+  before_action :set_movie  
+  before_action :require_signin
 
     def create
-        @movie = Movie.find(params[:movie_id])
+        
         @movie.favorites.create!(user: current_user)
       
         # or append to the through association 
@@ -11,9 +12,16 @@ class FavoritesController < ApplicationController
         redirect_to @movie, notice: "Thanks for fav'ing!"
       end
     def destroy
-        @movie = Movie.find(params[:movie_id])
+        
         favorite = current_user.favorites.find(params[:id])
         favorite.destroy
         redirect_to @movie, notice: "Sorry you unfaved it!"
       end
+
+      private
+
+      def set_movie
+        @movie = Movie.find_by!(slug: params[:id])
+    end
+
 end
